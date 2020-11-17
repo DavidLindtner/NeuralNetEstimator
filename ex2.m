2751131477e210d642585785f880bee371de90e5
David Lindtner <lindtnerdavid@gmail.com>
2020-11-17 17:24:02 +0000

    Exercise #2

---
+++ ex2.m
@@ -0,0 +1,16 @@
+function [data] = ex2()
+%ex2 Reads the database 'tpcda21_02_dataset.mat' and remove not important data from it
+%   Function removes 'u2' and calculate 'u67' (u67 = 0.4878 * u6 + 0.5261 * u7;)
+
+    data = load('tpcda21_02_dataset.mat');
+    
+    data.u67 = 0.4878 * data.u6 + 0.5261 * data.u7;
+    data = rmfield(data,'u2');
+    data = rmfield(data,'u6');
+    data = rmfield(data,'u7');
+    y = data.y;
+    data = rmfield(data,'y');
+    data.y = y;
+
+end
+
--- main.m
+++ main.m
@@ -2,21 +2,6 @@
 %%  Deadline 15.12.2020 23:59
 clear; close all; clc;
 
-%%  Work
-data = load('tpcda21_02_dataset.mat');
-
-figure(1)
-plot(data.u1)
-hold on
-plot(data.u2)
-plot(data.u3)
-plot(data.u4)
-plot(data.u5)
-plot(data.u6)
-plot(data.u7)
-
-figure(2)
-plot(data.y)
-
-
+%%  EX2 - Updating database
+data = ex2();
 
